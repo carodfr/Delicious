e8da05607f5d870a81e9c5162706cba3d1779f6a
carodfr <carlosalrofre@gmail.com>
2020-07-11 04:44:37 +0000

    Configuration to run gunicorn as TCP socket

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,7 +1,6 @@
 import multiprocessing
 
-#bind = "127.0.0.1:8000"
-bind = "unix:/run/gunicorn.sock"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
 #remember to set forwarded_allow_ips
