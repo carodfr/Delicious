d41211e1dc30982f00ef9ab247de95f45989dc45
carodfr <carlosalrofre@gmail.com>
2020-07-18 07:39:10 +0000

    Implementation of dotenv

--- www/app/__init__.py
+++ www/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 from app.db import db
 from app.views.productsBP import productsBP 
@@ -10,7 +11,12 @@ def create_app():
     # create and configure the app
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:TGdxctSoUtqbMoZaDLne@storedb.c8k7qd9rz8qw.eu-central-1.rds.amazonaws.com/storedb'
+    # load dotenv in the base root
+    APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
+    dotenv_path = os.path.join(APP_ROOT, '.env')
+    load_dotenv(dotenv_path)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['PROPAGATE_EXCEPTIONS'] = True
     app.secret_key = os.urandom(64)
